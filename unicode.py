f7c37cc7e87dc602f22fa054bb1959fc1b3b7cb4
James Gerity <snoopjedi@gmail.com>
2022-09-30 23:37:06 -0400

    Use 6-digit codepoint repr

--- unicode.py
+++ unicode.py
@@ -8,10 +8,10 @@ MAX_LEN = 16
 
 def _describe_char(bot, char):
     try:
-        bot.say(f"{char}: U+{ord(char):0>4x} ({unicodedata.category(char)}) {unicodedata.name(char)}")
+        bot.say(f"{char}: U+{ord(char):0>6x} ({unicodedata.category(char)}) {unicodedata.name(char)}")
     except ValueError:
         bot.say(
-            f"No info for U+{ord(char):0>4x} (I only know about Unicode up to {unicodedata.unidata_version})"
+            f"No info for U+{ord(char):0>5x} in Unicode {unicodedata.unidata_version}"
         )
 
 
