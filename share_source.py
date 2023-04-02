d1debe7e55a6464453b3826a02a78e6fc2aa59c7
James Gerity <snoopjedi@gmail.com>
2023-04-02 15:11:34 -0400

    Update source link

--- share_source.py
+++ share_source.py
@@ -3,4 +3,4 @@ from sopel import plugin
 
 @plugin.commands("source")
 def share_source(bot, trigger):
-    bot.say("Hack the planet ✊: https://github.com/SnoopJeDi/galois")
+    bot.say("Hack the planet ✊: https://git.snoopj.dev/SnoopJ/galois")
