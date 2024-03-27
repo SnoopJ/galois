7b1ad56402afe6f8d828ea67a1cd0c9197accc94
James Gerity <snoopjedi@gmail.com>
2024-03-26 20:13:20 -0400

    Synchronize how.py with running instance

--- how.py
+++ how.py
@@ -8,40 +8,57 @@ from sopel import plugin
 
 def howmany_rule(settings):
     prefix = settings.core.prefix
-    return [re.compile(rf"{prefix}how(\s?many|\s?much)?(\s*(\S+))?")]
+    return [re.compile(rf"{prefix}how(\s?many|\s?much)?(\s*(?P<thing>\S+))?")]
+
+
+
+CUSTOM_PLURALIZATIONS = {
+    "🎵": "🎶",
+    "🏠": "🏘️",
+}
+
+
+def _maybe_pluralize(thing: str) -> str:
+    custom_plural = CUSTOM_PLURALIZATIONS.get(thing)
+    import q; q.q(thing, custom_plural)
+    if custom_plural is not None:
+        plural = custom_plural
+    else:
+        try:
+            plural = Word(thing).pluralize()
+        except Exception as exc:
+            pass
+
+    if plural[-2:] == "ss":
+        # double-s plural is wrong more often than it's right, we failed to pluralize
+        return thing
+    else:
+        return plural
 
 
 @plugin.rule_lazy(howmany_rule)
 @plugin.example("!how much spam")
 def howmany(bot, trigger):
     """ Ask how many/how much of something """
-    if trigger.group(3):
-        thing_grp = trigger.group(3).lstrip()
+    thing_grp = trigger.group('thing')
+    if thing_grp:
+        thing_grp = thing_grp.strip()
     else:
         thing_grp = None
 
     qty = random.randint(0, 1000)
-    if thing_grp is not None:
-        # special case: !how many
-        if thing_grp is None:
-            bot.reply(str(qt))
-        # special case: !how many are [whatever predicate]?
-        elif thing_grp.startswith("are "):
-            bot.reply(str(qty))
-            return
+    if thing_grp is None:
+        thing = ""
+    # special case: !how many are [whatever predicate]?
+    elif thing_grp.startswith("are "):
+        bot.reply(str(qty))
+        return
+    else:
         thing = thing_grp.rstrip("?").split()[0]
 
-        if qty != 1 and thing.isalpha():
-            try:
-                plural = Word(thing).pluralize()
-            except Exception as exc:
-                pass
-            if plural[-2:] == "ss":
-                # double-s plural is wrong more often than it's right
-                pass
-            else:
-                thing = plural
+        if qty != 1:
+            thing = _maybe_pluralize(thing)
 
-    else:
-        thing = ""
-    bot.reply(f"{qty} {thing}")
+        thing = " " + thing
+
+    bot.reply(f"{qty}{thing}")
