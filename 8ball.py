f6ccbb9acef212a78388790dada9cba3d73f0f38
James Gerity <snoopjedi@gmail.com>
2025-02-27 22:07:23 -0500

    Commit 何 command

--- 8ball.py
+++ 8ball.py
@@ -3,15 +3,27 @@ import re
 
 from sopel import plugin
 
-responses = ["yes", "no"]
+RESPONSES = {
+    "en": {
+        True: ["yes"],
+        False: ["no"],
+    },
+    "ja": {
+        True: ["はい", "ええ"],
+        False: ["いいえ", "ない"],
+    },
+}
 
 
 @plugin.commands("8", "8ball")
 @plugin.example("!8 is this the real life?")
 def howmany(bot, trigger):
     """ Ask the bot a yes or no question """
+    ans = random.random() < 0.5
+    response = random.choice(RESPONSES["en"][ans])
+
     if not trigger.group(2):
-        bot.reply(random.choice(responses))
+        bot.reply(response)
         return
     else:
         text = trigger.group(2).lower()
@@ -19,4 +31,23 @@ def howmany(bot, trigger):
     if text.startswith("are you sure"):
         bot.reply("Did I stutter?")
     else:
-        bot.reply(random.choice(responses))
+        bot.reply(response)
+
+
+@plugin.commands("何", "nan", "nani")
+@plugin.example("!何　これは現実なのか？")
+def 何(bot, trigger):
+    """ Ask the bot a yes or no question """
+    ans = random.random() < 0.5
+    response = random.choice(RESPONSES["ja"][ans])
+
+    if not trigger.group(2):
+        bot.reply(response)
+        return
+    else:
+        text = trigger.group(2).lower()
+
+    if text.startswith("are you sure"):
+        response = "何でこのバカ！"
+
+    bot.reply(response)
